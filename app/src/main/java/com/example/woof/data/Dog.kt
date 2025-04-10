e4ca7d84d028dc6a8ec343cf1b87a25b5e218bf5
Santiard <jesardila17@gmail.com>
2025-04-10 10:02:24 -0500

    Update Dog.kt

--- app/src/main/java/com/example/woof/data/Dog.kt
+++ app/src/main/java/com/example/woof/data/Dog.kt
@@ -19,6 +19,7 @@ import android.content.Context
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import com.example.woof.R
+import androidx.compose.runtime.mutableStateListOf
 
 
 
@@ -49,28 +50,27 @@ fun addPilot (pilot : Pilot, pilots : MutableList<Pilot>){
 
 }
 
-var pilots = mutableListOf(
-
-    Pilot(R.drawable.max,R.string.pilot_name1,R.string.redbull),
-    Pilot(R.drawable.yuki,R.string.pilot_name2,R.string.redbull),
-    Pilot(R.drawable.lewis,R.string.pilot_name3,R.string.ferrari),
-    Pilot(R.drawable.charles,R.string.pilot_name4,R.string.ferrari),
-    Pilot(R.drawable.lando,R.string.pilot_name5,R.string.mclaren),
-    Pilot(R.drawable.oscar,R.string.pilot_name6,R.string.mclaren),
-    Pilot(R.drawable.george,R.string.pilot_name7,R.string.mercedes),
-    Pilot(R.drawable.kimi,R.string.pilot_name8,R.string.mercedes),
-    Pilot(R.drawable.fernando,R.string.pilot_name9,R.string.astonmartin),
-    Pilot(R.drawable.lance,R.string.pilot_name10,R.string.astonmartin),
-    Pilot(R.drawable.liam,R.string.pilot_name11,R.string.racingbulls),
-    Pilot(R.drawable.isack,R.string.pilot_name12,R.string.racingbulls),
-    Pilot(R.drawable.oliver,R.string.pilot_name13,R.string.haas),
-    Pilot(R.drawable.esteban,R.string.pilot_name14,R.string.haas),
-    Pilot(R.drawable.alex,R.string.pilot_name15,R.string.williams),
-    Pilot(R.drawable.carlos,R.string.pilot_name16,R.string.williams),
-    Pilot(R.drawable.pierre,R.string.pilot_name17,R.string.alpine),
-    Pilot(R.drawable.jack,R.string.pilot_name18,R.string.alpine),
-    Pilot(R.drawable.nico,R.string.pilot_name19,R.string.sauber),
-    Pilot(R.drawable.gabriel,R.string.pilot_name20,R.string.sauber)
+val pilots = mutableStateListOf(
+    Pilot(R.drawable.max, R.string.pilot_name1, R.string.redbull),
+    Pilot(R.drawable.yuki, R.string.pilot_name2, R.string.redbull),
+    Pilot(R.drawable.lewis, R.string.pilot_name3, R.string.ferrari),
+    Pilot(R.drawable.charles, R.string.pilot_name4, R.string.ferrari),
+    Pilot(R.drawable.lando, R.string.pilot_name5, R.string.mclaren),
+    Pilot(R.drawable.oscar, R.string.pilot_name6, R.string.mclaren),
+    Pilot(R.drawable.george, R.string.pilot_name7, R.string.mercedes),
+    Pilot(R.drawable.kimi, R.string.pilot_name8, R.string.mercedes),
+    Pilot(R.drawable.fernando, R.string.pilot_name9, R.string.astonmartin),
+    Pilot(R.drawable.lance, R.string.pilot_name10, R.string.astonmartin),
+    Pilot(R.drawable.liam, R.string.pilot_name11, R.string.racingbulls),
+    Pilot(R.drawable.isack, R.string.pilot_name12, R.string.racingbulls),
+    Pilot(R.drawable.oliver, R.string.pilot_name13, R.string.haas),
+    Pilot(R.drawable.esteban, R.string.pilot_name14, R.string.haas),
+    Pilot(R.drawable.alex, R.string.pilot_name15, R.string.williams),
+    Pilot(R.drawable.carlos, R.string.pilot_name16, R.string.williams),
+    Pilot(R.drawable.pierre, R.string.pilot_name17, R.string.alpine),
+    Pilot(R.drawable.jack, R.string.pilot_name18, R.string.alpine),
+    Pilot(R.drawable.nico, R.string.pilot_name19, R.string.sauber),
+    Pilot(R.drawable.gabriel, R.string.pilot_name20, R.string.sauber)
 )
 
 
